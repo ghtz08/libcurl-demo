a6cbf7cdb3e833cfb35412b7079452f52d93c263
TanZhou <tanzhou08@qq.com>
2021-06-14 20:45:27 +0800

    build: rename project and fix depend curl

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,4 +1,10 @@
 cmake_minimum_required(VERSION 3.12)
 # 3.12: target CURL::libcurl
 
+if (CURL_DIR)
+    set(PC_CURL_INCLUDE_DIRS "${CURL_DIR}/include")
+    set(PC_CURL_LIBRARY_DIRS "${CURL_DIR}/lib")
+endif()
 find_package(CURL REQUIRED)
+unset(PC_CURL_INCLUDE_DIRS)
+unset(PC_CURL_LIBRARY_DIRS)
